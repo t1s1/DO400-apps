03ba5fa951250486bc617563981c7b6c064c8734
Ted Singdahlsen <tsingdah@redhat.com>
2021-10-01 12:58:08 -0400

    finish exercise

---
+++ shopping-cart/src/test/java/com/redhat/shopping/integration/whitebox/ShoppingCartTest.java
@@ -0,0 +1,43 @@
+package com.redhat.shopping.integration.whitebox;
+
+import com.redhat.shopping.cart.CartService;
+import com.redhat.shopping.catalog.ProductNotFoundInCatalogException;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@QuarkusTest
+public class ShoppingCartTest {
+	@Inject
+	CartService cartService;
+
+	@BeforeEach
+	void clearCart() {
+		this.cartService.clear();
+	}
+
+	@Test
+	void addingProductThatIsInTheCartTheTotalItemsMatchTheSumOfQuantities() throws ProductNotFoundInCatalogException {
+		this.cartService.addProduct(1, 10);
+		this.cartService.addProduct(1, 100);
+
+		assertEquals(110, this.cartService.totalItems());
+	}
+
+	void addingNonExistingProductInCartTheTotalItemsMatchTheInitialQuantity() throws ProductNotFoundInCatalogException {
+		this.cartService.addProduct(1, 10);
+		assertEquals(10, this.cartService.totalItems());
+	}
+
+	void addingNonExistingProductInCatalogRaisesAnException() {
+		assertThrows(
+			ProductNotFoundInCatalogException.class,
+			() -> this.cartService.addProduct(9999, 10)
+			);
+	}
+}
